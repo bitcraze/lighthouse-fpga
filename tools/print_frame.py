9a9f41229009d2c6d32fb5e2d269df9812b2554a
Kristoffer Richardsson <kristoffer@bitcraze.io>
2020-04-03 16:35:52 +0200

    #6 Updated print_frame script with new terminology

--- tools/print_frame.py
+++ tools/print_frame.py
@@ -46,42 +46,39 @@ if __name__ == "__main__":
 
     print("Found sync!")
 
-    lastMeasurements = [0,] * 16
-
     reading = src.read(12)
+    prevSweepTime = 0
     while(len(reading) == 12):
         timestamp = struct.unpack("<I", reading[9:] + b'\x00')[0]
-        state = struct.unpack("<I", reading[6:9] + b'\x00')[0]
+        beam_word = struct.unpack("<I", reading[6:9] + b'\x00')[0]
         offset = struct.unpack("<I", reading[3:6] + b'\x00')[0]
-        reading = struct.unpack("<I", reading[:3] + b'\x00')[0]
+        first_word = struct.unpack("<I", reading[:3] + b'\x00')[0]
 
+        sensor = first_word & 0x03
+        width = (first_word >> 8) & 0xffff
 
-        sensor = reading & 0x03
-        identity = (reading >> 2) & 0x1f
-        mode = identity >> 1
-        found = ((reading >> 7) & 0x01) == 0
-        width = (reading >> 8) & 0xffff
+        nPoly_ok = ((first_word >> 7) & 0x01) == 0
+        identity = (first_word >> 2) & 0x1f
+        channel = identity >> 1
+        slow_bit = identity & 1
 
-        if state == 0xffffff:
+        # Sync frame, ignore it
+        if offset == 0xffffff:
             reading = src.read(12)
             continue
 
-        if (found):
-            print("Sensor: {}  TS:{:06x}  Width:{:4x}  Mode:{:2}({})  SyncTime:{:-6d}  BeamWord:{:05x}\t".format(sensor, timestamp, width, (identity >> 1) + 1,
-                  identity & 1, offset, state), end='')
+        # Detect new sweep
+        if ((timestamp - prevSweepTime) & 0xffffff) > 10000:
+            print()
+        prevSweepTime = timestamp
+
+        if (nPoly_ok):
+            print("Sensor: {}  TS:{:06x}  Width:{:4x}  Chan:{:2}({})  offset:{:-6d}  BeamWord:{:05x}\t".format(sensor, timestamp, width, channel + 1,
+                slow_bit, offset, beam_word), end='')
         else:
-            print("Sensor: {}  TS:{:06x}  Width:{:4x}  Mode: None  SyncTime:{:-6d}  BeamWord:{:05x}\t".format(sensor, timestamp, width, offset, state),
-                  end='')
+            print("Sensor: {}  TS:{:06x}  Width:{:4x}  Chan: None  offset:{:-6d}  BeamWord:{:05x}\t".format(sensor, timestamp, width, offset, beam_word),
+                end='')
 
-        if offset != 0:
-            if offset > (lastMeasurements[mode]+10000):
-                firstBeam = ((lastMeasurements[mode] * 8.0) / PERIODS[identity >> 1]) * 2 * math.pi
-                secondBeam = ((offset * 8.0) / PERIODS[identity >> 1]) * 2 * math.pi
-                azimuth, elevation = calculateAE(firstBeam, secondBeam)
-                print("Azimuth: {:3.3f}°, Elevation: {:3.3f}°".format(math.degrees(azimuth), math.degrees(elevation)), end='')
-
-            lastMeasurements[mode] = offset
-        
         print()
 
         reading = src.read(12)
